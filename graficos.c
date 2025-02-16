54651efdc0ad1b6294000a99d5963504d6cf7dc4
Tello-P <perezgarrote.tello@gmail.com>
2025-02-16 19:20:26 +0100

    Contador de comandos en cada mes

--- graficos.c
+++ graficos.c
@@ -7,6 +7,7 @@
 void titulo_inicio(const int num_comandos)
 {
 	system("clear");
+	
 	if (num_comandos == 0)
 	{
 		perror("No hay comandos usados todavia");
@@ -28,17 +29,56 @@ void titulo_inicio(const int num_comandos)
 }
 
 
-void graficos(const int num_comandos)
+void meses_barras(const int num_comandos, char *fechas_comandos[][10])
+{
+	
+	int en=0, fe=0, mz=0, ab=0, my=0, jn=0, jl=0, ag=0, sp=0, oc=0, no=0, di=0;
+
+
+	for (int i = 0; i < num_comandos; i++)
+	{	
+
+		char mes1 = fechas_comandos[i][0][5];  
+		char mes2 = fechas_comandos[i][0][6];  
+
+		if (mes1 == '0')
+		{
+			if (mes2 == '1') en++;
+			else if (mes2 == '2') fe++;
+			else if (mes2 == '3') mz++;
+			else if (mes2 == '4') ab++;
+			else if (mes2 == '5') my++;
+			else if (mes2 == '6') jn++;
+			else if (mes2 == '7') jl++;
+			else if (mes2 == '8') ag++;
+			else if (mes2 == '9') sp++;
+		}
+		else if (mes1 == '1')
+		{
+			if (mes2 == '0') oc++;
+			else if (mes2 == '1') no++;
+			else if (mes2 == '2') di++;
+		}
+	}
+
+}	
+
+
+
+void graficos(const int num_comandos, char *fechas_comandos[][10])
+
 {
 	titulo_inicio(num_comandos);	
-
+	meses_barras(num_comandos, fechas_comandos);
 }
 
 int main()
 {
 
-	int num_comandos = 24323;
-	graficos(num_comandos);
+	int num_comandos = 2;
+	char *fechas_comandos[][10] = {{"2025-11-4", "3"},{"2025-11-18", "8"},};
+
+	graficos(num_comandos, fechas_comandos);
 	
 
 	return 0;
